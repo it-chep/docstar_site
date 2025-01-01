befd9ee18b8ee869e22a8970b89406112a7adae8
necheporuk_ma <necheporuk_ma@sima-land.ru>
2025-01-01 11:14:36 +0300

    Фиксы админки + скрипт для очистки параметров

--- docstar_site/admin.py
+++ docstar_site/admin.py
@@ -4,13 +4,22 @@ from docstar_site.models import *
 
 
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code')
+    list_display = ("id", 'name', 'code')
+    search_fields = ("name",)
 
 
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'status_club', 'city', 'speciallity')
+    list_display = ("id", 'name', 'email', 'city', 'speciallity')
     search_fields = ("email", "name",)
     prepopulated_fields = {"slug": ("name",)}
+
+    list_filter = ('city', 'speciallity')
+    raw_id_fields = ('city', 'speciallity')
+
+
+class SpeciallityAdmin(admin.ModelAdmin):
+    list_display = ("id", 'name',)
+    search_fields = ("name",)
 
 
 class ExportAdmin(admin.ModelAdmin):
@@ -19,7 +28,8 @@ class ExportAdmin(admin.ModelAdmin):
 
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(City, CityAdmin)
-admin.site.register(Speciallity)
+admin.site.register(Speciallity, SpeciallityAdmin)
 
 from django.contrib.auth.models import Group
+
 admin.site.unregister(Group)
---
+++ docstar_site/management/commands/clean_params.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+
+from docstar_site.models import Doctor
+
+
+class Command(BaseCommand):
+    help = "Clean doctors params"
+
+    def handle(self, *args, **options):
+        doctors = Doctor.objects.all()
+        for doctor in doctors:
+            if doctor.additional_speciallity == "---":
+                doctor.additional_speciallity = None
+            if doctor.inst_url == "---":
+                doctor.inst_url = None
+            if doctor.vk_url == "---":
+                doctor.vk_url = None
+            if doctor.dzen_url == "---":
+                doctor.dzen_url = None
+            if doctor.tg_url == "---":
+                doctor.tg_url = None
+            if doctor.prodoctorov:
+                doctor.prodoctorov = None
+            if doctor.subscribers_inst:
+                doctor.subscribers_inst = None
+
+            doctor.save()

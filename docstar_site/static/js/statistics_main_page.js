797933ea64b9d445825c04794e1d3e43645e497b
pavel <ctop124@mail.ru>
2025-07-07 16:04:03 +0300

    feature. запрос на получение статистики по варчам и общее кол-во подписчиков в тг. fix верстка

--- docstar_site/static/js/statistics_main_page.js
+++ docstar_site/static/js/statistics_main_page.js
@@ -3,21 +3,29 @@
 
 
 function getStatistics() {
-
+    const loader = $('.statistics_loader')
     $.ajax({
         url: `/api/v1/settings/`,
         method: 'GET',
         success: function (response) {
+            const number_of_doctors_container = $('#number_of_doctors')
+            const number_of_subs_tg_container = $('#number_of_subs_tg')
 
-            console.log(response)
+            const target = $('.statistics')
 
+            loader.addClass('hidden')
+            target.addClass('show')
+
+            const {doctors_count, subscribers_count} = response;
+            number_of_doctors_container.text(`${doctors_count}`)
+            number_of_subs_tg_container.text(`${subscribers_count}`)
         },
         error: function (xhr, status, error) {
+            loader.remove()
             console.error('Ошибка при запросе статистики:', error);
         }
     });
 }
-
 
 $(document).ready(function () {
     getStatistics()
